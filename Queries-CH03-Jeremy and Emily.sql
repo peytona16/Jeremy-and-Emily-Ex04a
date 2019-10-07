818d4ac56105a31b7bfde19116243d4a80a63f54
JKueltzo <54907271+JKueltzo@users.noreply.github.com>
2019-10-07 12:15:55 -0500

    Create Queries-CH03-Jeremy and Emily.sql

---
+++ Queries-CH03-Jeremy and Emily.sql
@@ -0,0 +1,30 @@
+/* *** SQL-Query-CH03-01 *** */
+SELECT  ProjectID, ProjectName, Department, MaxHours, StartDate, EndDate 
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-02 *** */
+SELECT *
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-03 *** */
+SELECT ProjectName, Department, MaxHours
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-04 *** */
+SELECT ProjectName, MaxHours, Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-05 *** */
+SELECT Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-06 *** */
+SELECT DISTINCT Department
+FROM PROJECT;
+
+/* *** SQL-Query-CH03-07 *** */
+SELECT *
+FROM PROJECT
+WHERE Department = 'Finance';
+
+/* *** SQL-Query-CH03-08 *** */
