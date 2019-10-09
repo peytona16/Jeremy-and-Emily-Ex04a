ca3ea8285c0dfc1d80d6251f88902bbd9bc8b6b2
JKueltzo <54907271+JKueltzo@users.noreply.github.com>
2019-10-09 16:38:03 -0500

    Update Queries-CH03-Jeremy and Emily.sql

--- Queries-CH03-Jeremy and Emily.sql
+++ Queries-CH03-Jeremy and Emily.sql
@@ -250,12 +250,32 @@ GROUP BY  LastName, FirstName
 ORDER BY  LastName, FirstName;
 
 /* *** SQL-Query-CH03-50 *** */
+SELECT  FirstName, LastName, ProjectID, HoursWorked 
+FROM   EMPLOYEE AS E JOIN ASSIGNMENT AS A 
+     ON  E.EmployeeNumber = A.EmployeeNumber
+WHERE   HoursWorked > 50 
+ORDER BY  LastName, FirstName, ProjectID;
 
 /* *** SQL-Query-CH03-51 *** */
+SELECT   ProjectName, FirstName, LastName, 
+            HoursWorked 
+FROM   (EMPLOYEE AS E INNER JOIN ASSIGNMENT AS A 
+     ON  E.EmployeeNumber = A.EmployeeNumber)
+         INNER JOIN PROJECT AS P  
+              ON A.ProjectID = P.ProjectID 
+ORDER BY  P.ProjectID, A.EmployeeNumber;
 
 /* *** SQL-Query-CH03-52 *** */
+SELECT * 
+FROM PROJECT;
 
 /* *** SQL-Query-CH03-53 *** */
+SELECT  ProjectName, FirstName, LastName, HoursWorked 
+FROM    EMPLOYEE AS E JOIN ASSIGNMENT AS A 
+     ON  E.EmployeeNumber = A.EmployeeNumber    
+            JOIN  PROJECT AS P
+            ON A.ProjectID = P.ProjectID
+ORDER BY  P.ProjectID, A.EmployeeNumber;
 
 
 
