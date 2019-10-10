3ba015929d228f2b64cd4c6b6d3b5320cf7f44fd
JKueltzo <54907271+JKueltzo@users.noreply.github.com>
2019-10-10 18:11:39 -0500

    Update Queries-CH03-Jeremy and Emily.sql

--- Queries-CH03-Jeremy and Emily.sql
+++ Queries-CH03-Jeremy and Emily.sql
@@ -1,3 +1,202 @@
+USE wp;
+
+
+CREATE  TABLE DEPARTMENT(
+	DepartmentName		Char(35)		NOT NULL,
+	BudgetCode			Char(30)		NOT NULL,
+	OfficeNumber		Char(15)		NOT NULL,
+	DepartmentPhone		Char(12)		NOT NULL,
+	CONSTRAINT 			DEPARTMENT_PK 	PRIMARY KEY(DepartmentName)
+	);
+
+CREATE  TABLE EMPLOYEE(
+	EmployeeNumber		Int 			NOT NULL auto_increment,
+	FirstName			Char(25) 		NOT NULL,
+	LastName			Char(25) 		NOT NULL,
+	Department			Char(35)		NOT NULL DEFAULT 'Human Resources',
+	Position			Char(35)		NULL,
+	Supervisor			Int				NULL,
+	OfficePhone			Char(12)		NULL,
+	EmailAddress		VarChar(100)	NOT NULL UNIQUE,
+	CONSTRAINT 			EMPLOYEE_PK 	PRIMARY KEY(EmployeeNumber),
+	CONSTRAINT 			EMP_DEPART_FK	FOREIGN KEY(Department)
+							REFERENCES DEPARTMENT(DepartmentName)
+							ON UPDATE CASCADE,
+	CONSTRAINT			EMP_SUPER_FK	FOREIGN KEY (Supervisor)
+							REFERENCES EMPLOYEE (EmployeeNumber)						
+	);
+
+/* mysql does not support incrementing by other than 1, but does allow 
+user to specify a starting point by using an alter table statement as shown below 
+thus we cannot support identity(1000,100) like mssql
+we will have to insert values 1000, 1100, 1200 etc manually  */
+CREATE  TABLE PROJECT (
+	ProjectID			Int				NOT NULL auto_increment,
+	ProjectName			Char(50) 		NOT NULL,
+	Department			Char(35)		NOT NULL,
+	MaxHours			Numeric(8,2)	NOT NULL DEFAULT 100,
+    StartDate			Date			NULL,
+    EndDate				Date			NULL,
+    CONSTRAINT 			PROJECT_PK 		PRIMARY KEY (ProjectID),
+	CONSTRAINT 			PROJ_DEPART_FK	FOREIGN KEY(Department)
+							REFERENCES DEPARTMENT(DepartmentName)
+								ON UPDATE CASCADE
+    	);
+ALTER table PROJECT auto_increment = 1000;
+
+CREATE  TABLE ASSIGNMENT (
+   	ProjectID			Int	 			NOT NULL,
+	EmployeeNumber		Int	 			NOT NULL,
+    HoursWorked			Numeric(6,2)	NULL,
+   	CONSTRAINT 			ASSIGNMENT_PK 	PRIMARY KEY (ProjectID, EmployeeNumber),
+   	CONSTRAINT 			ASSIGN_PROJ_FK  FOREIGN KEY (ProjectID)
+							REFERENCES PROJECT (ProjectID)
+								ON UPDATE NO ACTION
+								ON DELETE CASCADE,
+    CONSTRAINT 			ASSIGN_EMP_FK   FOREIGN KEY (EmployeeNumber)
+							REFERENCES EMPLOYEE (EmployeeNumber)
+								ON UPDATE NO ACTION
+								ON DELETE NO ACTION
+ 	);
+  
+  INSERT INTO DEPARTMENT VALUES('Administration', 'BC-100-10', 'BLDG01-210', '360-285-8100');
+INSERT INTO DEPARTMENT VALUES('Legal', 'BC-200-10', 'BLDG01-220', '360-285-8200');
+INSERT INTO DEPARTMENT VALUES('Human Resources', 'BC-300-10', 'BLDG01-230', '360-285-8300');
+INSERT INTO DEPARTMENT VALUES('Finance', 'BC-400-10', 'BLDG01-110', '360-285-8400');
+INSERT INTO DEPARTMENT VALUES('Accounting', 'BC-500-10', 'BLDG01-120', '360-285-8405');
+INSERT INTO DEPARTMENT VALUES('Sales and Marketing', 'BC-600-10', 'BLDG01-250', '360-287-8500');
+INSERT INTO DEPARTMENT VALUES('InfoSystems', 'BC-700-10', 'BLDG02-210', '360-287-8600');
+INSERT INTO DEPARTMENT VALUES('Research and Development', 'BC-800-10', 'BLDG02-250', '360-287-8700');
+INSERT INTO DEPARTMENT VALUES('Production', 'BC-900-10', 'BLDG02-110', '360-287-8800');
+
+
+
+/*****   EMPLOYEE DATA   ********************************************************/
+/* note that MySQL allows null in the auto_increment field to generate the next value */
+
+INSERT INTO EMPLOYEE(FirstName, LastName, Department, Position, OfficePhone, EmailAddress) 
+	VALUES(
+	'Mary', 'Jacobs', 'Administration', 'CEO', '360-285-8110', 'Mary.Jacobs@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Rosalie', 'Jackson', 'Administration', 'Admin Assistant', 1,
+	'360-285-8120', 'Rosalie.Jackson@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Richard', 'Bandalone', 'Legal', 'Attorney', 1,
+	'360-285-8210', 'Richard.Bandalone@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'George', 'Smith', 'Human Resources', 'HR3', 1,
+	'360-285-8310', 'George.Smith@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Alan', 'Adams', 'Human Resources', 'HR1', 4,
+	'360-285-8320', 'Alan.Adams@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Ken', 'Evans', 'Finance', 'CFO', 1,
+	'360-285-8410', 'Ken.Evans@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Mary', 'Abernathy', 'Finance', 'FA3', 6,
+    '360-285-8420', 'Mary.Abernathy@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Tom', 'Caruthers', 'Accounting', 'FA2', 6,
+	'360-285-8430', 'Tom.Caruthers@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Heather', 'Jones', 'Accounting', 'FA2', 6,
+	'360-285-8440', 'Heather.Jones@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Ken', 'Numoto', 'Sales and Marketing', 'SM3', 1,
+	 '360-287-8510', 'Ken.Numoto@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Linda', 'Granger', 'Sales and Marketing', 'SM2', 10,
+	 '360-287-8520', 'Linda.Granger@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'James', 'Nestor', 'InfoSystems', 'CIO', 1,
+	'360-287-8610', 'James.Nestor@WP.com');
+INSERT INTO EMPLOYEE(FirstName, LastName, Department, Position, Supervisor, EmailAddress)
+	VALUES(
+	'Rick', 'Brown', 'InfoSystems', 'IS2', 12, 'Rick.Brown@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Mike', 'Nguyen', 'Research and Development', 'CTO', 1,
+	'360-287-8710', 'Mike.Nguyen@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Jason', 'Sleeman', 'Research and Development', 'RD3', 14,
+	'360-287-8720', 'Jason.Sleeman@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Mary', 'Smith', 'Production', 'OPS3', 1,
+	'360-287-8810', 'Mary.Smith@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'Tom', 'Jackson', 'Production', 'OPS2', 14,
+	'360-287-8820', 'Tom.Jackson@WP.com');
+INSERT INTO EMPLOYEE VALUES(
+	null,'George', 'Jones', 'Production', 'OPS2', 15,
+	'360-287-8830', 'George.Jones@WP.com');
+INSERT INTO EMPLOYEE(FirstName, LastName, Department, Position, Supervisor, EmailAddress)
+	 VALUES(
+	'Julia', 'Hayakawa', 'Production', 'OPS1', 15, 'Julia.Hayakawa@WP.com');
+INSERT INTO EMPLOYEE(FirstName, LastName, Department, Position, Supervisor, EmailAddress)
+	 VALUES(
+	'Sam', 'Stewart', 'Production', 'OPS1', 15, 'Sam.Stewart@WP.com');
+
+/*****   PROJECT DATA   *********************************************************/
+
+
+INSERT INTO PROJECT VALUES(
+	1000,'2017 Q3 Production Plan', 'Production', 100.00, '2017-05-10', '2017-06-15');
+INSERT INTO PROJECT VALUES(
+	1100,'2017 Q3 Marketing Plan', 'Sales and Marketing', 135.00, '2017-05-10', '2017-06-15');
+INSERT INTO PROJECT VALUES(
+	1200,'2017 Q3 Portfolio Analysis', 'Finance', 120.00, '2017-07-05', '2017-07-25' );
+INSERT INTO PROJECT VALUES(
+	1300,'2017 Q3 Tax Preparation', 'Accounting', 145.00, '2017-08-10', '2017-10-15');
+INSERT INTO PROJECT VALUES(
+	1400,'2017 Q4 Production Plan', 'Production', 100.00, '2017-08-10', '2017-09-15');
+INSERT INTO PROJECT VALUES(
+	1500,'2017 Q4 Marketing Plan', 'Sales and Marketing', 135.00, '2017-08-10', '2017-09-15');
+INSERT INTO PROJECT (ProjectID, ProjectName, Department, MaxHours, StartDate)				
+	VALUES(										
+	1600,'2017 Q4 Portfolio Analysis', 'Finance', 140.00, '2017-10-05');			
+
+
+/*****   ASSIGNMENT DATA   ******************************************************/
+
+INSERT INTO ASSIGNMENT VALUES(1000, 1, 30.0);
+INSERT INTO ASSIGNMENT VALUES(1000, 6, 50.0);
+INSERT INTO ASSIGNMENT VALUES(1000, 10, 50.0);
+INSERT INTO ASSIGNMENT VALUES(1000, 16, 75.0);
+INSERT INTO ASSIGNMENT VALUES(1000, 17, 75.0);
+INSERT INTO ASSIGNMENT VALUES(1100, 1, 30.0);
+INSERT INTO ASSIGNMENT VALUES(1100, 6, 75.0);
+INSERT INTO ASSIGNMENT VALUES(1100, 10, 55.0);
+INSERT INTO ASSIGNMENT VALUES(1100, 11, 55.0);
+INSERT INTO ASSIGNMENT VALUES(1200, 3, 20.0);
+INSERT INTO ASSIGNMENT VALUES(1200, 6, 40.0);
+INSERT INTO ASSIGNMENT VALUES(1200, 7, 45.0);
+INSERT INTO ASSIGNMENT VALUES(1200, 8, 45.0);
+INSERT INTO ASSIGNMENT VALUES(1300, 3, 25.0);
+INSERT INTO ASSIGNMENT VALUES(1300, 6, 40.0);
+INSERT INTO ASSIGNMENT VALUES(1300, 8, 50.0);
+INSERT INTO ASSIGNMENT VALUES(1300, 9, 50.0);
+INSERT INTO ASSIGNMENT VALUES(1400, 1, 30.0);
+INSERT INTO ASSIGNMENT VALUES(1400, 6, 50.0);
+INSERT INTO ASSIGNMENT VALUES(1400, 10, 50.0);
+INSERT INTO ASSIGNMENT VALUES(1400, 16, 75.0);
+INSERT INTO ASSIGNMENT VALUES(1400, 17, 75.0);
+INSERT INTO ASSIGNMENT VALUES(1500, 1, 30.0);
+INSERT INTO ASSIGNMENT VALUES(1500, 6, 75.0);
+INSERT INTO ASSIGNMENT VALUES(1500, 10, 55.0);
+INSERT INTO ASSIGNMENT VALUES(1500, 11, 55.0);
+INSERT INTO ASSIGNMENT VALUES(1600, 3, 20.0);
+INSERT INTO ASSIGNMENT VALUES(1600, 6, 40.0);
+INSERT INTO ASSIGNMENT VALUES(1600, 7, 45.0);
+INSERT INTO ASSIGNMENT VALUES(1600, 8, 45.0);
+
+
+
+
+
+
+
+>>Queries<<
+
+
 /* *** SQL-Query-CH03-01 *** */
 SELECT  ProjectID, ProjectName, Department, MaxHours, StartDate, EndDate 
 FROM PROJECT;
